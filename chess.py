1b2a7150fd19e06586fd0767d08e5b3b1d7b7bfc
tuhinaww <iceybubblegumtea@gmail.com>
2023-12-21 19:06:22 +0530

    Reset Game State to Initial or Supplied EPD Hash

--- chess.py
+++ chess.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 class Chess:
     def __init__(self, EPD='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -'):
         self.columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -5,3 +6,14 @@ class Chess:
         self.piece_notation = {'p': 1, 'n': 2, 'b': 3, 'r': 4, 'q': 5, 'k': 6}
         self.piece_names = {1: 'Pawn', 2: 'Knight', 3: 'Bishop', 4: 'Rook', 5: 'Queen', 6: 'King'}
         self.reset(EPD=EPD)
+        
+    def reset(self, EPD='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -'):
+        self.log = []
+        self.init_pos = EPD
+        self.EPD_table = {}
+        self.current_player = 1
+        self.castling = [1, 1, 1, 1]
+        self.en_passant = None
+        self.previous_move = None
+        self.board = [[0] * 8 for _ in range(8)]
+        self.load_EPD(EPD)
