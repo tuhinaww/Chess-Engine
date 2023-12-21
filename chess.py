1d8c3a54f9cdb3d9e0b372daa2510c45f798bdc7
tuhinaww <iceybubblegumtea@gmail.com>
2023-12-21 19:10:07 +0530

    Display Current Game State

--- chess.py
+++ chess.py
@@ -6,7 +6,7 @@ class Chess:
         self.piece_notation = {'p': 1, 'n': 2, 'b': 3, 'r': 4, 'q': 5, 'k': 6}
         self.piece_names = {1: 'Pawn', 2: 'Knight', 3: 'Bishop', 4: 'Rook', 5: 'Queen', 6: 'King'}
         self.reset(EPD=EPD)
-        
+
     def reset(self, EPD='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -'):
         self.log = []
         self.init_pos = EPD
@@ -17,3 +17,10 @@ class Chess:
         self.previous_move = None
         self.board = [[0] * 8 for _ in range(8)]
         self.load_EPD(EPD)
+
+    def display(self):
+        result = '  a b c d e f g h  \n  ----------------\n'
+        for i, row in enumerate(self.board):
+            result += f'{8 - i}|{" ".join(self.get_piece_notation(piece) for piece in row)}|{8 - i}\n'
+        result += '  ----------------\n  a b c d e f g h\n'
+        print(result)
