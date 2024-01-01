d71dc00aa7eb6a943a7655d3374414d8360100f1
tuhinaww <iceybubblegumtea@gmail.com>
2024-01-01 18:53:22 +0530

    Implemented forward method in model

---
+++ mcts.py
@@ -0,0 +1,9 @@
+import os
+import math
+import json
+import numpy
+import torch
+import random
+import pandas as pd
+from copy import deepcopy
+from concurrent.futures import ProcessPoolExecutor,as_completed
--- model/model.py
+++ model/model.py
@@ -16,3 +16,17 @@ class PositionalEncoding(nn.Module):
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
         self.register_buffer('pe', pe)
+
+    def forward(self, src):
+        src = self.encoder(src) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src) #Encoder memory
+        output = self.gelu(output)
+        output = self.decoder(output) #Linear layer
+        output = self.gelu(output)
+        output = self.flatten(output)
+        v = self.v_output(output) #Value output
+        v = self.softmax(v) #Get softmax probability
+        p = self.p_output(output) #Policy output
+        p = self.softmax(p) #Get softmax probability
+        return v, p
