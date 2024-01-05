339b9cb3ebd9e7953a27ed4873dcd304e8c98cdd
tuhinaww <iceybubblegumtea@gmail.com>
2024-01-05 08:09:10 +0530

    Refactor forward method in TransformerModel class

--- model/model.py
+++ model/model.py
@@ -5,6 +5,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TransformerModel(nn.Module):
+    
+    def forward(self, src):
+        src = self.encoder(src) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src) #Encoder memory
+        output = self.gelu(output)
+        output = self.decoder(output) #Linear layer
+        output = self.gelu(output)
+        output = self.flatten(output)
+        v = self.v_output(output) #Value output
+        v = self.softmax(v) #Get softmax probability
+        p = self.p_output(output) #Policy output
+        p = self.softmax(p) #Get softmax probability
+        return v, p
+    
     def __init__(self, sinp, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5, padding_idx=32):
         super(TransformerModel, self).__init__()
         from torch.nn import TransformerEncoder, TransformerEncoderLayer
@@ -22,25 +37,27 @@ class TransformerModel(nn.Module):
         self.v_output = nn.Linear(sinp,3) #Decode layer
         self.p_output = nn.Linear(sinp,4096) #Decode layer
         self.init_weights()
-
-    def forward(self, src):
-        src = self.encoder(src) * math.sqrt(self.ninp)
-        src = self.pos_encoder(src)
-        output = self.transformer_encoder(src) #Encoder memory
-        output = self.gelu(output)
-        output = self.decoder(output) #Linear layer
-        output = self.gelu(output)
-        output = self.flatten(output)
-        v = self.v_output(output) #Value output
-        v = self.softmax(v) #Get softmax probability
-        p = self.p_output(output) #Policy output
-        p = self.softmax(p) #Get softmax probability
-        return v, p
     
     def init_weights(self):
         initrange = 0.1
         self.encoder.weight.data.uniform_(-initrange, initrange)
         self.decoder.bias.data.zero_()
+    
+    def get_batch(source, x, y):
+        data = torch.tensor([])
+        v_target = torch.tensor([])
+        p_target = torch.tensor([])
+        for i in range(y):
+            #Training data
+            if len(source) > 0 and x+i < len(source):
+                d_seq = source[x+i][:len(source[x+i])-4099]
+                data = torch.cat((data, d_seq))
+                #Target data
+                v_seq = source[x+i][-3:]
+                v_target = torch.cat((v_target, v_seq))
+                p_seq = source[x+i][-4099:-3]
+                p_target = torch.cat((p_target, p_seq))
+        return data.reshape(min(y, len(source[x:])), len(source[0])-4099).to(torch.int64), v_target.reshape(min(y, len(source[x:])), 3).to(torch.float), p_target.reshape(min(y, len(source[x:])), 4096).to(torch.float)
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=5000):
